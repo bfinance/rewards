e67d236cde83044af8df6ee630a8334eeac8241e
"bfinance" <"valdez@colorai.co">
2022-05-03 15:27:05 -0400

    The crowd

---
+++ contracts/liontoken.sol
@@ -0,0 +1,14 @@
+
+   
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract LionToken is ERC20 {
+    
+      constructor() ERC20("Lion", "Lion") {
+        _mint(msg.sender, 10000000000000*(10** uint256(decimals())));
+    }
+}
